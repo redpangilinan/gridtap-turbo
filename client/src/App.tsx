2ad50042a6cbcc62f4cab9d5ae150eba0cf7ba80
redpangilinan <redpangilinan715@gmail.com>
2023-06-14 05:11:56 +0800

    Changed title to project name and added meta tags

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import Home from './pages/Home';
 import Game from './pages/Game';
 import Navbar from './components/Navbar';
@@ -6,6 +7,33 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <div className='flex flex-col min-h-screen bg-gray-800 text-gray-300'>
+      <Helmet>
+        <title>GridTap Turbo</title>
+        <meta
+          name='description'
+          content='Tap fast and climb the leaderboards in GridTap Turbo, a fast-paced grid-tapping challenge.'
+        />
+        <meta
+          name='keywords'
+          content='dont tap, donttap, grid tap, grid, tap, dont, tap, white, tiles, dont tap the white tile, gridtap, turbo'
+        />
+        <meta name='author' content='Red Pangilinan' />
+        <link rel='canonical' href='https://gridtap-turbo.vercel.app/' />
+        <meta property='og:title' content='GridTap Turbo' />
+        <meta
+          property='og:description'
+          content='Tap fast and climb the leaderboards in GridTap Turbo, a fast-paced grid-tapping challenge.'
+        />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://gridtap-turbo.vercel.app/' />
+        <meta property='og:image:alt' content='GridTap Turbo' />
+        <meta property='og:site_name' content='GridTap Turbo' />
+        <meta name='twitter:title' content='GridTap Turbo' />
+        <meta
+          name='twitter:description'
+          content='Tap fast and climb the leaderboards in GridTap Turbo, a fast-paced grid-tapping challenge.'
+        />
+      </Helmet>
       <Navbar />
       <BrowserRouter>
         <Routes>
