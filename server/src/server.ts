6ddf8a9684c02318d2bbfa2de43d4da4fd965083
redpangilinan <redpangilinan715@gmail.com>
2023-06-14 20:49:23 +0800

    Implemented rate limiting

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import usersRouter from './routes/users';
-import { validateApiKey } from './middleware/validateApi';
 
 const app = express();
 const port = 3000;
@@ -9,8 +9,15 @@ app.get('/', (req, res) => {
   res.send('Home');
 });
 
+// Rate limit middleware
+const limiter = rateLimit({
+  windowMs: 1000,
+  max: 5,
+  message: 'Too many requests, please try again later.',
+});
+
 // Routes
-app.use('/users', validateApiKey, usersRouter);
+app.use('/users', limiter, usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
