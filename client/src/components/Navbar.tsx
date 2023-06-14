b572bcec594d4cbd1a2aa38239f94af7bfadfd2f
redpangilinan <redpangilinan715@gmail.com>
2023-06-15 05:33:37 +0800

    Removed shadow

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <nav className='w-full bg-gray-900 shadow'>
+    <nav className='w-full bg-gray-900'>
       <div className='justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8'>
         <div>
           <div className='flex items-center justify-between py-3 md:py-5 md:block'>
