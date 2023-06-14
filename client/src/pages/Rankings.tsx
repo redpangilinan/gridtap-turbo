74e41eacc8fb3d902d43ca15d49fa72494619ec2
redpangilinan <redpangilinan715@gmail.com>
2023-06-15 05:33:18 +0800

    Created ranking page that shows player stats

---
+++ client/src/pages/Rankings.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import RankTable from '../components/RankTable';
+import LoadingTable from '../components/LoadingTable';
+
+type TableData = {
+  user_id: number;
+  username: string;
+  level: number;
+  exp_percent: number;
+  scores: number;
+  top_score: number;
+};
+
+const Rankings: React.FC = () => {
+  const [userData, setUserData] = useState<TableData[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 350));
+
+        const apiUrl = `${import.meta.env.VITE_BASE_URL}/users`;
+        const response = await axios.get(apiUrl);
+
+        setUserData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  return (
+    <div className='flex-grow container mx-auto px-2'>
+      <h1 className='text-xl font-bold my-3'>Rankings</h1>
+      {isLoading ? <LoadingTable /> : <RankTable data={userData} />}
+    </div>
+  );
+};
+
+export default Rankings;
