9f53e6e9ed2e3206821bd214de0739ea04b7c312
redpangilinan <redpangilinan715@gmail.com>
2023-06-15 06:58:38 +0800

    Changed apiUrl

--- client/src/pages/Rankings.tsx
+++ client/src/pages/Rankings.tsx
@@ -21,7 +21,7 @@ const Rankings: React.FC = () => {
       try {
         await new Promise((resolve) => setTimeout(resolve, 350));
 
-        const apiUrl = `http://localhost:3001/users`;
+        const apiUrl = `${import.meta.env.REACT_APP_BASE_URL}/users`;
         const response = await axios.get(apiUrl);
 
         setUserData(response.data);
