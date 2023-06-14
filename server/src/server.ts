1f4bfdb1297496f1aad7aa1b6254a44934218b27
redpangilinan <redpangilinan715@gmail.com>
2023-06-15 05:11:08 +0800

    Forbid access to API outside of client domain if environment is production

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import rateLimit from 'express-rate-limit';
+import { config } from 'dotenv';
 import usersRouter from './routes/users';
 
+config();
 const app = express();
-const port = 3000;
+const port = 3001;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Home');
@@ -15,6 +19,22 @@ const limiter = rateLimit({
   max: 5,
   message: 'Too many requests, please try again later.',
 });
+
+// CORS configuration
+if (process.env.NODE_ENV === 'production') {
+  const allowedOrigins = [process.env.DOMAIN];
+
+  app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    } else {
+      res.status(403).send('Forbidden access');
+      return;
+    }
+    next();
+  });
+}
 
 // Routes
 app.use('/users', limiter, usersRouter);
