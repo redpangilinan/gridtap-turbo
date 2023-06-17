c461c7afadd66738018331e617764a32cd9ac83e
redpangilinan <redpangilinan715@gmail.com>
2023-06-17 08:42:30 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
+import HelmetTags from './components/HelmetTags';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Login from './pages/Login';
@@ -7,38 +7,12 @@ import Register from './pages/Register';
 import Game from './pages/Game';
 import Rankings from './pages/Rankings';
 
-function App() {
+const App = () => {
   return (
     <div className='flex flex-col min-h-screen bg-gray-900 text-gray-300'>
-      <Helmet>
-        <title>Gridtap Turbo</title>
-        <meta
-          name='description'
-          content='Tap fast and climb the leaderboards in GridTap Turbo, a fast-paced grid-tapping challenge.'
-        />
-        <meta
-          name='keywords'
-          content='dont tap, donttap, grid tap, grid, tap, dont, tap, white, tiles, dont tap the white tile, gridtap, turbo'
-        />
-        <meta name='author' content='Red Pangilinan' />
-        <link rel='canonical' href='https://gridtap-turbo.vercel.app/' />
-        <meta property='og:title' content='GridTap Turbo' />
-        <meta
-          property='og:description'
-          content='Tap fast and climb the leaderboards in GridTap Turbo, a fast-paced grid-tapping challenge.'
-        />
-        <meta property='og:type' content='website' />
-        <meta property='og:url' content='https://gridtap-turbo.vercel.app/' />
-        <meta property='og:image:alt' content='GridTap Turbo' />
-        <meta property='og:site_name' content='GridTap Turbo' />
-        <meta name='twitter:title' content='GridTap Turbo' />
-        <meta
-          name='twitter:description'
-          content='Tap fast and climb the leaderboards in GridTap Turbo, a fast-paced grid-tapping challenge.'
-        />
-      </Helmet>
-      <Navbar />
       <BrowserRouter>
+        <HelmetTags />
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
@@ -49,6 +23,6 @@ function App() {
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
