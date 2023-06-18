cd2e80ef7b5d85918be36bc740f02454afaa2618
redpangilinan <redpangilinan715@gmail.com>
2023-06-18 22:52:21 +0800

    Added tokenData and submitScore on users api

--- client/src/api/users.ts
+++ client/src/api/users.ts
@@ -1,6 +1,31 @@
 import axios from 'axios';
 
+type ScoreProps = {
+  score: number;
+  accuracy: number;
+  maxCombo: number;
+  hits: number;
+  miss: number;
+  device: string;
+};
+
+// Get all the users
 export const getUsers = () => {
   const apiUrl = `${import.meta.env.VITE_BASE_URL}/users`;
   return axios.get(apiUrl).then((res) => res.data);
+};
+
+// Get user data from JWT token
+export const tokenData = () => {
+  const apiUrl = `${import.meta.env.VITE_BASE_URL}/cookies`;
+  return axios.get(apiUrl, { withCredentials: true }).then((res) => res.data);
+};
+
+// Score submission
+export const submitScore = async (data: ScoreProps) => {
+  const token = await tokenData();
+  const userId = token.userId;
+  const postData = { ...data, userId };
+  const apiUrl = `${import.meta.env.VITE_BASE_URL}/scores`;
+  await axios.post(apiUrl, postData);
 };
