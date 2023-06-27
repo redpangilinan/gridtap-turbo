a36825ec464c6f811f0156714fee4c859a6f981b
redpangilinan <redpangilinan715@gmail.com>
2023-06-27 10:00:38 +0800

    Prevent scrolling on mobile when tapping the tiles

--- client/src/components/pages/Game/Tiles.tsx
+++ client/src/components/pages/Game/Tiles.tsx
@@ -28,7 +28,7 @@ const Tiles: React.FC<TileProps> = ({ device, isBlack, onClick }) => {
       <div
         className={`relative ${
           isBlack ? 'bg-black' : isClicked ? 'bg-red-500' : 'bg-white'
-        } w-24 h-24 md:w-32 md:h-32 flex items-center justify-center cursor-crosshair select-none border duration-100`}
+        } w-24 h-24 md:w-32 md:h-32 flex items-center justify-center cursor-crosshair select-none touch-none border duration-100`}
         onTouchStart={handleClick}
       >
         <div className='absolute w-full h-full bg-white rounded-full opacity-0 pointer-events-none'></div>
@@ -40,7 +40,7 @@ const Tiles: React.FC<TileProps> = ({ device, isBlack, onClick }) => {
     <div
       className={`relative ${
         isBlack ? 'bg-black' : isClicked ? 'bg-red-500' : 'bg-white'
-      } w-24 h-24 md:w-32 md:h-32 flex items-center justify-center cursor-crosshair select-none border duration-100`}
+      } w-24 h-24 md:w-32 md:h-32 flex items-center justify-center cursor-crosshair select-none touch-none border duration-100`}
       onMouseDown={handleClick}
     >
       <div className='absolute w-full h-full bg-white rounded-full opacity-0 pointer-events-none'></div>
